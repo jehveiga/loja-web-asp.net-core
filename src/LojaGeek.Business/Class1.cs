83ac7773a404c80d5a6cfbf2793217a18303364f
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-06-16 09:47:52 -0300

    Criando as dvisões da appliacação

---
+++ src/LojaGeek.Business/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaGeek.Business
+{
+    public class Class1
+    {
+    }
+}
