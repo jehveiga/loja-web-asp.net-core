21c47a0323caf45e231a6a0436206b5505123fae
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-08-20 15:40:46 -0300

    adicionando as imagens Demo

--- src/LojaGeek.App/Configurations/IdentityConfig.cs
+++ src/LojaGeek.App/Configurations/IdentityConfig.cs
@@ -1,4 +1,6 @@
 ﻿using LojaGeek.App.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -10,6 +12,13 @@ namespace LojaGeek.App.Configurations
     {
         public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     configuration.GetConnectionString("DefaultConnection")));
